8cd284b1b4edb5d06b2af28e90cce6353a3ed880
Sven Skyth Henriksen <78769573+Sven-Skyth-Henriksen@users.noreply.github.com>
2021-04-14 12:56:03 +0200

    Add files via upload

---
+++ pages/about.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+
+def write():
+    st.title('⚜️ About ⚜️:')
+    st.markdown('''
+    
+    Books are disponible for everyone, rich and poor, young and old. 
+    From love stroies to Sci Fi, it's gonna change the world for 
+    everyone. How will your life be affected ?📖
+    
+   ⬇️⬇️ This Project was created by 4 ***[Strive School]***(https://strive.school/) Students.⬇️⬇️
+    ''')
+    
+    st.markdown('## Get to know us 👋🏻:')
+    
+    if st.button('Click here'):
+        st.write('***The Developer Team***:')
+        st.write('• Sven Skyth Henriksen: [GitHub ](https://github.com/Sven-Skyth-Henriksen)&[ LinkedIn](https://www.linkedin.com/in/sven-skyth-henriksen-4857bb1a2/)')
+        st.write('• Madina Zhenisbek: [GitHub ](https://github.com/madinach)&[ LinkedIn](https://www.linkedin.com/in/madina-zhenisbek/)')
+        st.write('• Olatunde Salami: [GitHub ](https://github.com/salamituns)&[ LinkedIn](https://www.linkedin.com/in/olatunde-salami/)')
+        st.write('• Paramveer Singh: [GitHub ](https://github.com/paramveer)&[ LinkedIn](https://www.linkedin.com/in/paramveer-singh07/)')
+        
+        
+if __name__ == "__main__":
+    main()
