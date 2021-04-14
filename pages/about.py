affe0e9af3cd5bd96fa2cc7bed07d6fdeaada68c
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2021-04-14 13:59:39 +0200

    changes

--- pages/about.py
+++ pages/about.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
 
-def write():
-    st.title('⚜️ About ⚜️:')
-    st.markdown('''
+st.title('⚜️ About ⚜️:')
+st.markdown('''
     
     Books are disponible for everyone, rich and poor, young and old. 
     From love stroies to Sci Fi, it's gonna change the world for 
@@ -12,15 +11,12 @@ def write():
    ⬇️⬇️ This Project was created by 4 ***[Strive School]***(https://strive.school/) Students.⬇️⬇️
     ''')
     
-    st.markdown('## Get to know us 👋🏻:')
+st.markdown('## Get to know us 👋🏻:')
     
-    if st.button('Click here'):
-        st.write('***The Developer Team***:')
-        st.write('• Sven Skyth Henriksen: [GitHub ](https://github.com/Sven-Skyth-Henriksen)&[ LinkedIn](https://www.linkedin.com/in/sven-skyth-henriksen-4857bb1a2/)')
-        st.write('• Madina Zhenisbek: [GitHub ](https://github.com/madinach)&[ LinkedIn](https://www.linkedin.com/in/madina-zhenisbek/)')
-        st.write('• Olatunde Salami: [GitHub ](https://github.com/salamituns)&[ LinkedIn](https://www.linkedin.com/in/olatunde-salami/)')
-        st.write('• Paramveer Singh: [GitHub ](https://github.com/paramveer)&[ LinkedIn](https://www.linkedin.com/in/paramveer-singh07/)')
+if st.button('Click here'):
+    st.write('***The Developer Team***:')
+    st.write('• Sven Skyth Henriksen: [GitHub ](https://github.com/Sven-Skyth-Henriksen)&[ LinkedIn](https://www.linkedin.com/in/sven-skyth-henriksen-4857bb1a2/)')
+    st.write('• Madina Zhenisbek: [GitHub ](https://github.com/madinach)&[ LinkedIn](https://www.linkedin.com/in/madina-zhenisbek/)')
+    st.write('• Olatunde Salami: [GitHub ](https://github.com/salamituns)&[ LinkedIn](https://www.linkedin.com/in/olatunde-salami/)')
+    st.write('• Paramveer Singh: [GitHub ](https://github.com/paramveer)&[ LinkedIn](https://www.linkedin.com/in/paramveer-singh07/)')
         
-        
-if __name__ == "__main__":
-    main()
