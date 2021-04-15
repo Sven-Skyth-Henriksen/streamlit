829747e1524a6704005d9c816ff68a2a0ac5ba3d
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2021-04-15 22:27:38 +0200

    Update about.py

--- apps/about.py
+++ apps/about.py
@@ -39,6 +39,12 @@ def app():
             
         
     with image:
-        image = Image.open('book.jpeg')
-        st.image(image , caption='Quote by ERNEST HEMINGWAY')
-        
+        #image = Image.open('book.jpeg')
+        #st.image(image , caption='Quote by ERNEST HEMINGWAY')
+        col1, col2, col3 = st.beta_columns([2,1,1])
+    with col1:
+        st.image("book.jpeg")
+    with col2:
+        st.write("")
+    with col3:
+        st.write("")
