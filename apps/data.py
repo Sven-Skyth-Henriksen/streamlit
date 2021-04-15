99bc26b20b6d1eedb879fa3f0a655183036ba0a9
Sven Skyth Henriksen <78769573+Sven-Skyth-Henriksen@users.noreply.github.com>
2021-04-15 13:00:32 +0200

    Add files via upload

---
+++ apps/data.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+def app():
+    
+    data = st.beta_container()
+    
+
+    df = pd.read_csv('Books_total.csv')
+
+    with data:
+        st.title('Data')
+        st.markdown("![Data](https://media4.giphy.com/media/xT9C25UNTwfZuk85WP/200.webp?cid=ecf05e47844brv5239cczg9hqo5ernebyirvx4xaua7k2dk8&rid=200.webp&ct=g)")
+        if st.checkbox('View Data'):
+            st.subheader('Raw Data')
+            st.write(df.head(50))
+
+    
+    
+
+    
+       
+    
