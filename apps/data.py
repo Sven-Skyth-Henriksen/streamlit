26605e212d65eb73eab12c03513d11e52511d80c
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2021-04-16 01:47:40 +0200

    Update data.py

--- apps/data.py
+++ apps/data.py
@@ -12,8 +12,8 @@ def app():
     with data:
         st.title('Data')
         st.markdown("![Data](https://media4.giphy.com/media/xT9C25UNTwfZuk85WP/200.webp?cid=ecf05e47844brv5239cczg9hqo5ernebyirvx4xaua7k2dk8&rid=200.webp&ct=g)")
-        if st.checkbox(''):
-            st.subheader('Snippet of the Data')
+        if st.checkbox('Snippet of the Data'):
+            st.subheader('First 50 Datas')
             st.write(df.header(50))
         
         if st.checkbox('All the Data'):
