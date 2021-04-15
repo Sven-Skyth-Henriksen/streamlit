a5a704ef55fbae31ad919e389b08c734041f29e2
Sven Skyth Henriksen <78769573+Sven-Skyth-Henriksen@users.noreply.github.com>
2021-04-15 13:02:06 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+
+import streamlit as st
+from multiapp import MultiApp
+from apps import about, data, models, home # import your app modules here
+
+app = MultiApp()
+
+st.title(' Ravenclaw Team welcomes you to our website')
+st.text('')
+st.text('')
+st.text('')
+st.markdown("![Alt Text](https://media4.giphy.com/media/lKYMj63WqlBcc/giphy.gif?cid=ecf05e470d15qsjwvus5fhfgb3l2hpf5js7gqr26lshesrpe&rid=giphy.gif&ct=g)")
+
+
+
+st.markdown('''
+# Book Project
+
+Please select a page:
+
+''')
+
+# Add all your application here
+app.add_app('Home',home.app)
+app.add_app("About", about.app)
+app.add_app("Data", data.app)
+app.add_app('Models', models.app)
+
+
+
+# The main app
+app.run()
+
