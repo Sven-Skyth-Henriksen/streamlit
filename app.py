5f5edd482173fd598b573443fac5fdd567ff2d4e
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2021-04-16 09:32:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+
 import streamlit as st
 from multiapp import MultiApp
-from apps import about, data, models, home, graph  # import your app modules here
+from apps import about, data, home, graph  # import your app modules here
 
 app = MultiApp()
 
@@ -8,19 +9,22 @@ st.title(' Ravenclaw Team welcomes you to our website')
 st.text('')
 st.text('')
 st.text('')
-
 st.markdown("![Alt Text](https://media4.giphy.com/media/lKYMj63WqlBcc/giphy.gif?cid=ecf05e470d15qsjwvus5fhfgb3l2hpf5js7gqr26lshesrpe&rid=giphy.gif&ct=g)")
 
 
 
-st.markdown('''## 🔥 Best books you really have to read atleast once 🔥 
-Please select a page:''')
+st.markdown('''
+## 🔥 Best books you really have to read once 🔥
+
+Please select a page:
+
+''')
 
 # Add all your application here
 app.add_app('Home',home.app)
 app.add_app("About", about.app)
 app.add_app("Data", data.app)
-app.add_app('Models', models.app)
+#app.add_app('Models', models.app)
 app.add_app('Graphs', graph.app)
 
 
