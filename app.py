3695d3cf1742cf103d0300ee3e5a1965d230123d
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2021-04-16 11:37:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,20 +5,28 @@ from apps import about, data, home, graph  # import your app modules here
 
 app = MultiApp()
 
-st.title(' Ravenclaw Team welcomes you to our website')
-st.text('')
-st.text('')
-st.text('')
-st.markdown("![Alt Text](https://media4.giphy.com/media/lKYMj63WqlBcc/giphy.gif?cid=ecf05e470d15qsjwvus5fhfgb3l2hpf5js7gqr26lshesrpe&rid=giphy.gif&ct=g)")
+header = st.beta_container()
+meme = st.beta_container()
+subtitle = st.beta.container()
+selection = st.beta.container()
 
 
 
-st.markdown('''
-## 🔥 Best books you really have to read once 🔥
+with header:
+    st.title(' Ravenclaw Team welcomes you to our website')
+    st.title('')
+    st.markdown('')
+    st.subheader('')
+    
+with meme:
+    st.markdown("![Alt Text](https://media4.giphy.com/media/lKYMj63WqlBcc/giphy.gif?cid=ecf05e470d15qsjwvus5fhfgb3l2hpf5js7gqr26lshesrpe&rid=giphy.gif&ct=g)")
 
-Please select a page:
 
-''')
+with subtitle:
+    st.markdown('## 🔥 Best books you really have to read once 🔥 ')
+## 🔥 Best books you really have to read once 🔥 
+with selection:
+    st.markdown('Please select a page:')
 
 # Add all your application here
 app.add_app('Home',home.app)
