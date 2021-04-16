6dee39dcdef9d75f2481ba0a0a343f82648fc709
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2021-04-16 15:15:10 +0200

    a

--- apps/data.py
+++ apps/data.py
@@ -16,8 +16,8 @@ def app():
         st.title('Data')
         st.markdown("![Data](https://media4.giphy.com/media/xT9C25UNTwfZuk85WP/200.webp?cid=ecf05e47844brv5239cczg9hqo5ernebyirvx4xaua7k2dk8&rid=200.webp&ct=g)")
 
-        if st.checkbox('All the Data'):
-            st.subheader('Raw Data')
+        if st.checkbox('Reveal The Library'):
+            st.subheader('Books')
             st.write(df) #.header(50) inside the the ()
     
                     
