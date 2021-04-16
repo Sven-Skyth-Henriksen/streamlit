74fdf605ba90e57990b77e1296d9399d1c9a323f
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2021-04-16 11:47:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ app = MultiApp()
 
 
 st.title(' Ravenclaw Team welcomes you to our website')
-   
-    
+st.text('')
+st.text('')
+st.text('')
 
 st.markdown("![Alt Text](https://media4.giphy.com/media/lKYMj63WqlBcc/giphy.gif?cid=ecf05e470d15qsjwvus5fhfgb3l2hpf5js7gqr26lshesrpe&rid=giphy.gif&ct=g)")
 
